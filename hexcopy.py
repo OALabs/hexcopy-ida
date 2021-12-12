be1fe739564655cf10d04523e93c41870ef5200c
Heros <1131602048@trakya.edu.tr>
2021-12-12 13:11:28 +0300

    Update hexcopy.py
    I made fixes for use without Qt libraries.

--- hexcopy.py
+++ hexcopy.py
@@ -26,28 +26,14 @@ import sys
 import idc
 import idaapi
 import idautils
+from subprocess import check_call
 
 major, minor = map(int, idaapi.get_kernel_version().split("."))
 using_ida7api = (major > 6)
-using_pyqt5 = using_ida7api or (major == 6 and minor >= 9)
-
-if using_pyqt5:
-    import PyQt5.QtGui as QtGui
-    import PyQt5.QtCore as QtCore
-    import PyQt5.QtWidgets as QtWidgets
-    from PyQt5.Qt import QApplication
-
-else:
-    import PySide.QtGui as QtGui
-    import PySide.QtCore as QtCore
-    QtWidgets = QtGui
-    QtCore.pyqtSignal = QtCore.Signal
-    QtCore.pyqtSlot = QtCore.Slot
-    from PySide.QtGui import QApplication
-
 
 def copy_to_clip(data):
-    QApplication.clipboard().setText(data)
+    cmd='echo '+data.strip()+'|clip'
+    return check_call(cmd, shell=True)
 
 
 def PLUGIN_ENTRY():
